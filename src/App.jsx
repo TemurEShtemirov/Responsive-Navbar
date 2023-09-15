748d7707f58470673157c799b3a0a374553919b3
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-09-15 18:58:27 +0500

    Done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Navbar'
+export default function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  )
+}
